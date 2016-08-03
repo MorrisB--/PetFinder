f9df3b118d3fd6f7908639c2fe5e8ecdbc13505b
MorrisB-- <morris.ballenger@yahoo.com>
2016-08-03 18:55:11 -0500

    Added Tess' files

---
+++ profile.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class profile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void addButton_Click(object sender, EventArgs e)
+    {
+        petLabel2.Visible = true;
+        petImage2.Visible = true;
+        trackButton2.Visible = true;
+   
+    }
+    protected void trackButton1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("tracking.aspx");
+
+    }
+    protected void trackButton2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("tracking.aspx");
+    }
+}
